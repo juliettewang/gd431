6f1ef2193d7ef1cb897bc5db345f0646d61d20bc
Juliette Wang <jwang05@mica.edu>
2018-11-01 21:58:43 -0400

    nav javascript

---
+++ zenhappens/src/js/menu.js
@@ -0,0 +1,20 @@
+
+
+var navButton = document.getElementById("js-nav-trigger");
+
+navButton.addEventListener("click", function(){
+    // change aria-expanded
+    let expanded = this.getAttribute("aria-expanded") === "true" || false;
+    this.setAttribute("aria-expanded", !expanded);
+    // console.log(expanded); - tests if it works
+    // change data-visible
+    let menu = document.getElementById("js-site-nav");
+    let visible = menu.dataset.visible === "true" || false;
+    menu.dataset.visible = !visible;
+    // hiding body after full screen menu
+    if (!visible === true) {
+        document.body.style.overflowY = "hidden";
+    } else{
+        document.body.removeAttribute("style");
+    }
+})
